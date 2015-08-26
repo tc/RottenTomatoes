3fe31967a2354ee1c0e7c079e8f62008e0b1e7a8
Tommy Chheng <tommy@tumblr.com>
2015-08-26 12:05:11 -0700

    Initial Commit

---
+++ RottenTomatoes/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RottenTomatoes
+//
+//  Created by Tommy Chheng on 8/26/15.
+//  Copyright (c) 2015 Tommy Chheng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
